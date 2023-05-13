00a8cfa9698654b380cee749e83da192330f8e3e
Harpratapsinh Nakoom <91936753+HarpratapsinhNakoom@users.noreply.github.com>
2023-05-13 16:10:37 +0530

    sidebar resposnsiveness

--- Home Page/script.js
+++ Home Page/script.js
@@ -2,13 +2,15 @@ const timeline_area = document.getElementById("page4-timeline");
 const prevButton = document.getElementById("timeline-prev")
 const nextButton = document.getElementById("timeline-next")
 
+let counter = Math.floor((270*12 + 90)/(parseInt(window.innerWidth)));
+let window_width = window.innerWidth;
 
 let timeline_counter = 0;
 const goPrevTimeline = () => {
     if(timeline_counter === 0) return;
     timeline_counter--;
-    timeline_area.style.translate = `calc(${timeline_counter}*(220px - 100vw))`;
-
+    timeline_area.style.translate = `-${(timeline_counter)*window_width}px`;
+    
     if(timeline_counter === 0) {
         prevButton.style.display = "none";
         nextButton.style.display = "block";
@@ -19,11 +21,11 @@ const goPrevTimeline = () => {
 }
 
 const goNextTimeline = () => {
-    if(timeline_counter === 2) return;
+    if(timeline_counter === counter) return;
     timeline_counter++;
-    timeline_area.style.translate = `calc(${timeline_counter}*(220px - 100vw))`;
-
-    if(timeline_counter === 2) {
+    timeline_area.style.translate = `-${timeline_counter*window_width}px`;
+    // console.log(parseInt(window.innerWidth));
+    if(timeline_counter === counter) {
         nextButton.style.display = "none";
         prevButton.style.display = "block";
     }else {
@@ -31,3 +33,15 @@ const goNextTimeline = () => {
         prevButton.style.display = "block";
     }
 }
+
+
+const changeCounter = () => {
+    // console.log(window.innerWidth)
+    counter = Math.floor((270*12 + 90)/(parseInt(window.innerWidth)));
+    window_width = window.innerWidth;
+}
+window.addEventListener('resize', changeCounter);
+
+// item - 270px
+// total - device width
+// count - 12
--- common js/navbarJs.js
+++ common js/navbarJs.js
@@ -3,3 +3,21 @@ $(document).ready(function(){
 		$(this).toggleClass('open');
 	});
 });
+
+const toggleBtn = document.getElementById("navbar-icon");
+const sidebar = document.getElementById("sidebar-links");
+toggleBtn.addEventListener("click", () => {
+	// console.log(sidebar.style.translate);
+	if(sidebar.style.translate === "none") {
+		sidebar.style.translate = "100vw";
+	} else{
+		sidebar.style.translate = "none";
+	}
+})
+
+window.addEventListener("resize", () => {
+	if(parseInt(window.innerWidth) >= 1040) {
+		sidebar.style.translate = "50vw";
+		toggleBtn.style.display = "none";
+	}
+})
